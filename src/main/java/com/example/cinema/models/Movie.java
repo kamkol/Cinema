bc2ce0d59d2d728d86a69179835f21b1679537e9
Kamil Koltan <kam.koltan@gmail.com>
2023-06-20 17:57:14 +0200

    adding develope branch

--- src/main/java/com/example/cinema/models/Movie.java
+++ src/main/java/com/example/cinema/models/Movie.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -17,5 +18,10 @@ public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String title;
+
+    private String description;
+
 
 }
